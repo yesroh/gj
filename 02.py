2bbe11bf12d7930a84d23321072082b900240d8d
노은선 <115079913+yesroh@users.noreply.github.com>
2023-11-11 18:05:14 +0900

    Update 02.py

--- 02.py
+++ 02.py
@@ -88,7 +88,6 @@ AI와 SW기술에 많은 흥미를 가지고 있습니다.''')
 기존의 문제를 효과적으로 해결하는일을 좋아합니다. ''')
   col1.link_button('Go to my github',url='https://github.com/minkyungjoo08/-')
   #col2.image("사진.jpg")
-
   st.divider() 
 
   col1, col2, col3 = st.columns(3)
