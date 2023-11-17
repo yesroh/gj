1c3aec322881b758217cd9057bb82096e39004cc
노은선 <115079913+yesroh@users.noreply.github.com>
2023-11-18 02:05:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import pandas as pd
 import folium
 from streamlit_folium import st_folium
 
-
 tab1,tab2,tab3=st.tabs(['Projects','Description','Introduction'])
 
 with tab1:
   df=pd.read_csv("war_data.csv")
-  df1=df.drop(['내용','등록자','시기','출처','승','패'],axis=1)
-  df2=df1.drop(['인물'],axis=1)
+  df_f=pd.read_csv("first.csv")
+  df1=df.drop(['내용','등록자','출처','승','패'],axis=1) #
+  df2=df.drop(['내용','등록자','시기','출처','승','패','인물'],axis=1)
   st.subheader('데이터 수집')
-  st.text('1950~51년 사이의 전투 데이터 N개를 수집')
-  st.dataframe(df1) #제목,지역,인물(df)
+  st.text('1950~51년 사이의 6.25전쟁 데이터 150여개를 수집')
+  st.dataframe(df_f) # 원시 df 
+
+  st.subheader('데이터 전처리')
+  st.text('시기나 출처가 불분명한 데이터 삭제, 가공하기 쉽게 일부 수정')
+  st.dataframe(df1) #가공한 df
 
   battle_df=df2.groupby('지역').count().reset_index()
   battle_df.rename(columns={'제목':'격전횟수'},inplace=True)
@@ -31,9 +35,10 @@ with tab1:
   st.bar_chart(battle_df,x='지역',y='격전횟수')
 
   map_geo = folium.Map(location=[35.5666,126.9784], zoom_start=7)
-  #st_data=st_folium(map_geo,width=700, height=1000) #지도
+  #st_data=st_folium(map_geo,width=700, height=1000)#지도
 
   geo_json = 'divided_by_korea_city.json'
+
 
   folium.Choropleth(geo_data=geo_json,
                  data=battle_df,
@@ -43,10 +48,9 @@ with tab1:
                  legend_name="격전횟수"
                  ).add_to(map_geo)
   st.subheader('지역별 격전 횟수 시각화')
-  st_map=st_folium(map_geo,width=700, height=1000 )#전투 시각화 지도
-
+  st_map=st_folium(map_geo,width=700, height=1000 )#시각화 지도
+  
   st.subheader('날짜별 격전의 승패 그래프')
-  #st.text('9/14일 이후로 승승장구하는 모습을 보임.')
   st.bar_chart(data=df,x='시기',y=['승','패'],color=['#ff0000','#0000ff']) 
 
 
@@ -62,12 +66,7 @@ with tab2:
 이 전투에서 패배를 하게되면 북의 통일이 되는 상황이기에 가장 중요한 전투였습니다.
 또한 다부동 전투를 이끌던 백선엽 장군의 명언 "내가 후퇴하면 나를 쏴라"라는 말을 해 장병들의 사기를 돋았습니다.''')
   col2.image('다부동전투.jpeg')
-  col1  ,col2 = st.columns([2,1])
-  col1.subheader('인천상륙작전')
-  col1.text('''맥아더 장군의 지휘하에 서울 탈환 작전의 밑거름이 되었고,
-5000/1의 확률로 성공한 이 작전이 북한군과의 전세를 뒤바꾸는 중요한 전투였습니다.
-또한 이 전투를 통해 38선을 넘어 북진을 하게 만드는 변곡점같은 전투였습니다.''')
-  col2.image('인천상륙작전.jpeg')
+ 
   col1  ,col2 = st.columns([2,1]) 
   col1.subheader('장진호전투')
   col1.text('''북에서 펼쳐진 가장 큰 싸움으로 이 전투는 북한군이 한반도 북부로의 진군을 중단시키려는 시도와 
@@ -75,6 +74,13 @@ with tab2:
 유엔군이 중공군에 포위되어 격전을 벌여 승리하였고,이 전투를 기점으로 량걍도 지역까지 진출하는 계기가 되었으며
 중공군과 북한군에 큰 피해를 입혔습니다.''')
   col2.image('장진호전투.jpeg')
+
+  col1  ,col2 = st.columns([2,1])
+  col1.subheader('인천상륙작전')
+  col1.text('''맥아더 장군의 지휘하에 서울 탈환 작전의 밑거름이 되었고,
+5000/1의 확률로 성공한 이 작전이 북한군과의 전세를 뒤바꾸는 중요한 전투였습니다.
+또한 이 전투를 통해 38선을 넘어 북진을 하게 만드는 변곡점같은 전투였습니다.''')
+  col2.image('인천상륙작전.jpeg')
   
   # 6.25 전쟁에 대해 조사해보면서 든 견해 ()
   # ex. 역사를 잊은 민족에게 미래는 없다라는 말과 같이 역사, 즉 인류 사회의 변천 데이터를 수집하여 
